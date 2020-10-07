f4a9942f7c906f3fc60ef97e6bc97cb92b11b629
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-07 12:21:46 +0700

    Add files via upload

---
+++ tugas3_1.php
@@ -0,0 +1,7 @@
+<?php
+	$jarak = 360;
+	$kecepatan = 90;
+	$waktu = ($jarak/$kecepatan)*60;
+
+	echo "Jarak Kota A - Z = $jarak km<br>Kecepatan motor = $kecepatan km/jam<br>Waktu Tempuh = ? <br>Jawaban :<br> $jarak : $kecepatan x 60 = $waktu menit";
+?>
